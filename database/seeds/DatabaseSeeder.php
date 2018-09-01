01a191f7a67c5fba229d58cb89ef82df6f51c1ea
Alex Renoki <rennokki@gmail.com>
2018-09-01 16:15:13 +0300

    Updated seeders.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use TCG\Voyager\Traits\Seedable;
 
 class DatabaseSeeder extends Seeder
 {
+    use Seedable;
+
+    protected $seedersPath = __DIR__.'/';
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
     /**
      * Seed the application's database.
      *
