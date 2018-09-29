69e906b23021ee3af7a120edef8612bcc17f5884
Alex Renoki <rennokki@gmail.com>
2018-09-29 13:19:12 +0300

    Fixed the authentication code with safer methods.

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -50,37 +50,17 @@ class AuthenticationController extends Controller
 
         $socialite = Socialite::driver($social)->stateless()->user();
         $userSocial = UserSocial::{$social}()->socialId($socialite->getId())->first();
-        $sessionedUser = User::with(['facebook', 'google', 'instagram'])->find(Session::get('user_id'));
+        $sessionedUser = User::with($social)->find(Session::get('user_id'));
 
         Session::forget('user_id');
 
-        if ($userSocial) {
-            $user = $userSocial->user()->first();
-
-            if ($sessionedUser) {
-                if (! $user->is($sessionedUser)) {
-                    return redirect(route('me'))->with('alert', 'Contul de '.ucfirst($social).' este deja asociat cu alt cont.');
-                }
-            }
-
-            \Auth::login($user);
-
-            $userSocial->update([
-                'token' => $socialite->token,
-                'token_expiry' => ($socialite->expiresIn) ? now()->addSeconds($socialite->expiresIn) : null,
-                'socialite' => $socialite,
-            ]);
-
-            if (! $user->canApplyToCampaigns()) {
-                return redirect(route('me').'?open=profile');
-            }
-
-            return redirect(route('me'))->with('success', 'Bine ai revenit în contul tău!');
-        }
-
         if ($sessionedUser) {
             if ($sessionedUser->{$social}) {
-                return redirect(route('me'))->with('alert', 'Contul de '.ucfirst($social).' este deja asociat contului!');
+                return redirect(route('me'))->with('alert', 'Profilul de '.ucfirst($social).' este deja asociat contului!');
+            }
+
+            if ($userSocial) {
+                return redirect(route('me'))->with('alert', 'Profilul  de '.ucfirst($social).' este deja asociat cu alt cont.');
             }
 
             $sessionedUser->socials()->create([
@@ -104,7 +84,25 @@ class AuthenticationController extends Controller
             return redirect(route('me'))->with('success', 'Contul de '.ucfirst($social).' a fost adăugat!');
         }
 
-        $user = User::with(['facebook', 'google', 'instagram'])->email($socialite->getEmail())->first();
+        if ($userSocial) {
+            $user = $userSocial->user()->first();
+
+            $userSocial->update([
+                'token' => $socialite->token,
+                'token_expiry' => ($socialite->expiresIn) ? now()->addSeconds($socialite->expiresIn) : null,
+                'socialite' => $socialite,
+            ]);
+
+            \Auth::login($user);
+
+            if (! $user->canApplyToCampaigns()) {
+                return redirect(route('me').'?open=profile');
+            }
+
+            return redirect(route('me'))->with('success', 'Bine ai revenit în contul tău!');
+        }
+
+        $user = User::with($social)->email($socialite->getEmail())->first();
 
         if (! $user) {
             $user = User::create([
@@ -114,12 +112,6 @@ class AuthenticationController extends Controller
                 'avatar' => asset('/images/profile/patrick.png'),
             ]);
 
-            $user->load($social);
-            
-            \Auth::login($user);
-        }
-
-        if (! $user->{$social}) {
             $user->socials()->create([
                 'social_id' => $socialite->getId(),
                 'social_type' => $social,
@@ -131,7 +123,7 @@ class AuthenticationController extends Controller
                 'token_expiry' => ($socialite->expiresIn) ? now()->addSeconds($socialite->expiresIn) : null,
                 'socialite' => $socialite,
             ]);
-
+            
             \Auth::login($user);
 
             if (! $user->canApplyToCampaigns()) {
