c917b1e75db021bccb053c6322459b72cb9da23e
Alex Renoki <rennokki@gmail.com>
2018-10-29 19:54:56 +0200

    Added icon for campaigns.

--- resources/views/campaigns.blade.php
+++ resources/views/campaigns.blade.php
@@ -119,7 +119,12 @@
                             </div>
                           </div>
                         @endif
-                        <img alt="{{ $campaign->name }}" src="{{ $campaign->imageUrl() }}" class="img-fluid rounded" style="height: 130px; min-height: 130px;">
+                        @if($campaign->imageUrl())
+                          <img alt="{{ $campaign->name }}" src="{{ $campaign->imageUrl() }}" class="img-fluid rounded" style="height: 130px; min-height: 130px;">
+                        @endif
+                        @if($campaign->mdi_icon)
+                          <div class="text-center text-{{ $campaign->color_scheme }} mt-3"><i class="mdi mdi-{{ $campaign->mdi_icon }} mdi-48px"></i></div>
+                        @endif
                         <div class="card-body py-2">
                           <h5 class="text-{{ $campaign->color_scheme }} text-uppercase mt-3">{{ $campaign->name }}</h5>
                           <div class="danger-text">
